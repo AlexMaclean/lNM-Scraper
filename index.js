ce5382d806dd48b30f8d66d65d295268918a592f
Alex MacLean <alex.maclean2000@gmail.com>
2019-05-05 22:17:03 -0700

    comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const prompt = require('./prompt');
 
 const BASE_URL = 'https://www.navcen.uscg.gov';
 const RECENT_COUNT = 2;
+const CLEAN = false;
 
 function getRegionUrl(region) {
     return `${BASE_URL}/?pageName=lnmDistrict&region=${region}`
@@ -18,38 +19,66 @@ function getFilename(pdf) {
     return pdf.substr(pdf.lastIndexOf('/') + 1)
 }
 
+/**
+ * Opens a request to the page containing all the LNM PDF file links for
+ * a given region. Scrapes through the page for the hrefs of the valid
+ * PDF links.
+ *
+ * @param url  Address of the region page to open
+ * @returns {Promise<Array>} A possibly empty array of all the pdf hrefs found
+ */
 function scrapeForPdfs(url) {
     return new Promise(function (resolve) {
-        console.log("Scarping '" + url + "' for LNM PDFs");
+        console.log(`Scarping '${url}' for LNM PDFs`);
         request(url, (error, response, body) => {
             if (error) {
-                console.log("...Error encountered: " + error.toString());
+                console.log(`...Error encountered: ${error.toString()}`);
                 resolve([]);
             }
             if (response.statusCode !== 200) {
-                console.log("...Error encountered: " + response.statusCode);
+                console.log(`...Error encountered: ${response.statusCode}`);
                 resolve([]);
             }
             const $ = cheerio.load(body);
-            console.log("...Successfully loaded page");
+            console.log(`...Successfully loaded page`);
 
             let links = [];
 
             $('a[href*="lnm"][href*=".pdf"]:contains("PDF")').each(function (i) {
-                const href = $(this).attr("href");
+                const href = $(this).attr('href');
                 links[i] = href;
-                console.log("...(" + i + ") " + href)
+                console.log(`...(${i}) ${href}`)
             });
             resolve(links)
         })
     })
 }
 
+/**
+ * Given a list of all the PDF files found, filters it down to only the
+ * most recent n. Because of the format of the pdf filenames this is very simple
+ * and can be accomplished just by a lexicographical sort.
+ *
+ * @param paths  All the paths to pdf files found on the region page
+ * @param count  The top count to return, the rest are discarded
+ * @returns {Uint16Array}  An array of the most recent elements to download
+ */
 function filterRecent(paths, count) {
-    return paths.sort().reverse().slice(0, count);
+    const recent = paths.sort().reverse().slice(0, count);
+    console.log(`Querying the ${count} most recent LNMs`);
+    recent.forEach((pdf) => {
+        console.log(`...${pdf}`);
+    });
+    return recent;
 }
 
-async function checkDownloaded() {
+/**
+ * Opens the current working directory and filters all the PDF files
+ * found inside. Used to ensure a file is not downloaded too many times
+ *
+ * @returns {Promise<Array>} A list of strings representing filenames
+ */
+async function alreadyDownloaded() {
     let downloaded = [];
     await new Promise(resolve => {
         fs.readdir('.', function (err, items) {
@@ -65,6 +94,22 @@ async function checkDownloaded() {
 }
 
 /**
+ * Checks to determine if a given file should be downloaded based on
+ * whether it already exists and clean mode is enabled
+ *
+ * @param filename    The file which could potentially be downloaded
+ * @param downloaded  Array of files that have already been downloaded
+ * @returns {boolean} Indicating whether the file should be downloaded
+ */
+function shouldDownload(filename, downloaded) {
+    if (!CLEAN && downloaded.includes(filename)) {
+        console.log(`[${filename}] already found, not re-downloading`);
+        return false;
+    }
+    return true;
+}
+
+/**
  * Method to download a pdf from the net using the request library, given a
  * url and a filename to download to. Logs relevant information to the console
  * and does most of the necessary error checking
@@ -76,26 +121,26 @@ function downloadPdf(url, filename) {
 
     const optionsStart = {
         uri: url,
-        encoding: "binary",
+        encoding: 'binary',
         headers: {
-            "Content-type": "applcation/pdf"
+            'Content-type': 'applcation/pdf'
         }
     };
 
-    console.log("Attempting to download '" + url + "'");
+    console.log(`[${filename}] Downloading from '${url}'`);
 
     request(optionsStart, (error, response, body) => {
         if (error) {
-            console.log("...Error encountered: " + error.toString());
+            console.log(`...[${filename}] Error encountered: ${error.toString()}`);
         } else if (response.statusCode !== 200) {
-            console.log("...Error encountered: " + response.statusCode);
+            console.log(`...[${filename}] Error encountered: ${response.statusCode}`);
         } else {
-            console.log("...Successfully connected");
+            console.log(`...[${filename}] Successfully connected`);
             let writeStream = fs.createWriteStream(filename);
-            console.log("...Writing to file '" + writeStream.path + "'");
+            console.log(`...[${filename}] Writing to file`);
             writeStream.write(body, 'binary');
             writeStream.on('finish', () => {
-                console.log("...All data successfully written");
+                console.log(`...[${filename}] All data successfully written`);
             });
             writeStream.end();
         }
@@ -106,10 +151,10 @@ async function main() {
     const region = await prompt();
     const allPaths = await scrapeForPdfs(getRegionUrl(region));
     const recent = filterRecent(allPaths, RECENT_COUNT);
-    const downloaded = await checkDownloaded();
+    const downloaded = await alreadyDownloaded();
     recent.forEach((pdf) => {
         const filename = getFilename(pdf);
-        if (!downloaded.includes(filename)) {
+        if (shouldDownload(filename, downloaded)) {
             downloadPdf(getPdfUrl(pdf), filename)
         }
     })
--- prompt.js
+++ prompt.js
@@ -13,18 +13,26 @@ const promptRegion = () => {
     })
 };
 
-module.exports = () => {
-    return new Promise(async function (resolve) {
-        while (true) {
-            const answer = await promptRegion();
-            const region = parseInt(answer);
-            if (isNaN(region) || region <= 0) {
-                stdio.write(`Invalid district '${answer}'\n`)
-            } else {
-                stdio.close();
-                resolve(region);
-                break;
-            }
+/**
+ * Prompts the user for a region, if the input is clearly invalid,
+ * such as not an int or negative the user is asked again for region.
+ *
+ * @param resolve  Used to return the integer from the Promise
+ */
+async function prompt(resolve) {
+    while (true) {
+        const answer = await promptRegion();
+        const region = parseInt(answer);
+        if (isNaN(region) || region <= 0) {
+            stdio.write(`Invalid district '${answer}'\n`)
+        } else {
+            stdio.close();
+            resolve(region);
+            break;
         }
-    })
+    }
+}
+
+module.exports = () => {
+    return new Promise(prompt);
 };
